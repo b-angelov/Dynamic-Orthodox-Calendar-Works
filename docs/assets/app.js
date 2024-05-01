93bec4cba4ec61cf28db4ef41eb330f9dc47deb8
Borislav Angelov <98764167+b-angelov@users.noreply.github.com>
2024-05-01 23:55:36 +0300

    updating pages

--- docs/assets/app.js
+++ docs/assets/app.js
@@ -6,6 +6,27 @@ function logics(){
     const templateRow = tableBodyElement.querySelector("tr")
     const modes = ["JulianInGregorian","GaussJulianInGregorian","GaussGregorian","AnonymousGregorian","AnonymousGregorianOptimized","Julian","GaussJulian"]
     templateRow.remove()
+    let lang = "bg"
+
+    const translations = {
+        bg:{
+            year: "година",
+            day: "ден",
+            month:"месец",
+            calendar:"календар",
+            leap:"високосна",
+            JulianInGregorian:"юлиански в грегориански",
+            GaussJulianInGregorian:"юлиански в грегориански - гаус",
+            GaussGregorian:" гргориански - гаус",
+            AnonymousGregorian:"анонимен грегориански",
+            AnonymousGregorianOptimized:" анонимен грегориански - оптимизиран",
+            Julian:"юлиански",
+            GaussJulian:"юлиански - гаус",
+            months:[null,"януари","февруари","март","април","май","юни","юли","август","септември","октомври","ноември","декември"],
+            bools:{true:"✔",false:"X"}
+
+        }
+    }
 
     function addRow(year,month,day,calendar,leap){
         const newRow = templateRow.cloneNode(true)
@@ -16,7 +37,8 @@ function logics(){
 
     function addData(year,mode){
         const {month,day,leap} = new EasterCalculus(year,mode)
-        addRow(year,month,day,mode.match(/[A-Z]?[a-z]*/gm).filter(el=>el).join(" "),leap)
+        // addRow(year,month,day,mode.match(/[A-Z]?[a-z]*/gm).filter(el=>el).join(" "),leap)
+        addRow(year,translations[lang].months[month],day,translations[lang][mode],translations[lang].bools[leap])
     }
 
     modes.forEach(mode=>addData(Number(yearHolder.textContent),mode))
